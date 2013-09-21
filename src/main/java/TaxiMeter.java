c596000ba5b7894eb79e2bc95625607195956656
Liu Jing <liujing92684@163.com>
2013-09-21 22:29:51 +0800

    calculate the price when distance is in base distance and beyond base distance

--- src/main/java/TaxiMeter.java
+++ src/main/java/TaxiMeter.java
@@ -1,5 +1,12 @@
 public class TaxiMeter {
+
     public double calculate(double mile) {
-        return 0;  //To change body of created methods use File | Settings | File Templates.
+        if (mile < 2) {
+            return 6;
+        } else if (mile <= 8) {
+            return 6 + 0.8 * (mile - 2);
+        } else {
+            return 6 + 0.8 * 6 + 1.2 * (mile - 6);
+        }
     }
 }
--- src/test/java/TaxiMeterTest.java
+++ src/test/java/TaxiMeterTest.java
@@ -1,19 +1,32 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class TaxiMeterTest {
-    TaxiMeter taxiMeter;
+    public TaxiMeter taxiMeter;
 
-    @Test
+    @Before
     public void setUp() throws Exception {
         taxiMeter = new TaxiMeter();
     }
 
     @Test
     public void should_charge_6_within_2_miles() throws Exception {
-             double price = taxiMeter.calculate(2);
-             assertThat(price, is(6.0));
+        double price = taxiMeter.calculate(2);
+        assertThat(price, is(6.0));
+    }
+
+    @Test
+    public void should_charge_more_than_6_when_distance_is_3() throws Exception {
+        double price = taxiMeter.calculate(3);
+        assertThat((price - 6) > 0, is(true));
+    }
+
+    @Test
+    public void should_charge_12_when_distance_is_9() throws Exception {
+        double price = taxiMeter.calculate(9);
+        assertThat(price, is(14.4));
     }
 }
