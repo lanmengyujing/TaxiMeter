7355a3f96bc220622a9290ccc844b6493dd95c73
Liu Jing <liujing92684@163.com>
2013-09-21 22:46:55 +0800

    add stopping time

--- src/main/java/TaxiMeter.java
+++ src/main/java/TaxiMeter.java
@@ -5,17 +5,19 @@ public class TaxiMeter {
     public static final double BASE_DISTANCE = 8;
     public static final double BASE_PRICE = 0.8;
     public static final double RATING_PRICE = 1.2;
+    public static final double STOPPING_PRICE = 0.25;
 
-    public double calculate(double mile) {
-        double distancePrice = 0;
+    public double calculate(double mile, int stoppingTime) {
+        double distanceCharge = 0;
+        double stoppingCharge = stoppingTime * STOPPING_PRICE;
         if (InStartDistance(mile)) {
-            distancePrice = STARTING_PRICE;
+            distanceCharge = STARTING_PRICE;
         } else if (InBaseDistance(mile)) {
-            distancePrice += STARTING_PRICE + BASE_PRICE * (mile - STARTING_DISTANCE);
+            distanceCharge += STARTING_PRICE + BASE_PRICE * (mile - STARTING_DISTANCE);
         } else {
-            distancePrice += STARTING_PRICE + BASE_PRICE * STARTING_PRICE + RATING_PRICE * (mile - STARTING_PRICE);
+            distanceCharge += STARTING_PRICE + BASE_PRICE * STARTING_PRICE + RATING_PRICE * (mile - STARTING_PRICE);
         }
-        return distancePrice;
+        return distanceCharge + stoppingCharge;
     }
 
     private boolean InBaseDistance(double mile) {
--- src/test/java/TaxiMeterTest.java
+++ src/test/java/TaxiMeterTest.java
@@ -14,21 +14,25 @@ public class TaxiMeterTest {
 
     @Test
     public void should_charge_6_within_2_miles() throws Exception {
-        double price = taxiMeter.calculate(2);
+        double price = taxiMeter.calculate(2, 0);
         assertThat(price, is(6.0));
     }
 
     @Test
     public void should_charge_more_than_6_when_distance_is_3() throws Exception {
-        double price = taxiMeter.calculate(3);
+        double price = taxiMeter.calculate(3, 0);
         assertThat((price - 6) > 0, is(true));
     }
 
     @Test
     public void should_charge_12_when_distance_is_9() throws Exception {
-        double price = taxiMeter.calculate(9);
+        double price = taxiMeter.calculate(9, 0);
         assertThat(price, is(14.40));
     }
 
-
+    @Test
+    public void should_charge_7_when_distance_is_2_and_stopping_time_is_4_minutes() throws Exception {
+        double price = taxiMeter.calculate(2,4);
+        assertThat(price, is(7.0));
+    }
 }
